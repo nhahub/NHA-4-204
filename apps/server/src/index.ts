abc39dc79d569c95796e4e89af679e2c0d7a43dc
Maged Ibrahim <magedibra@yahoo.com>
2026-02-21 15:36:44 +0200

    Init monorepo

---
+++ apps/server/src/index.ts
@@ -0,0 +1,39 @@
+import { createContext } from "@careergps/api/context";
+import { appRouter } from "@careergps/api/routers/index";
+import { auth } from "@careergps/auth";
+import { env } from "@careergps/env/server";
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
+import { toNodeHandler } from "better-auth/node";
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: env.CORS_ORIGIN,
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
+
+app.all("/api/auth{/*path}", toNodeHandler(auth));
+
+app.use(
+  "/trpc",
+  createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  }),
+);
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.status(200).send("OK");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
---
+++ packages/api/src/routers/index.ts
@@ -0,0 +1,5 @@
+import { router } from "../index";
+
+export const appRouter = router({});
+
+export type AppRouter = typeof appRouter;
